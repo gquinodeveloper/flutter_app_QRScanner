e4c9636a6c554d71724cdca59db22e5317bf9267
Gustavo Quino <31318966+gquinodeveloper@users.noreply.github.com>
2021-06-11 11:59:09 -0500

    Update utils.dart

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,5 +1,6 @@
 import 'package:url_launcher/url_launcher.dart';
 
+//Esta función muestra un preview de las direcciones URL
 launchURL({String url}) async {
   if (await canLaunch(url)) {
     await launch(url);
