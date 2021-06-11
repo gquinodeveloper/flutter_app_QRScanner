e351b719d9a975acfe1f7dee2a98e3f8016d74ce
Gustavo Quino <31318966+gquinodeveloper@users.noreply.github.com>
2021-06-11 11:58:25 -0500

    Update scanner_model.dart

--- lib/models/scanner_model.dart
+++ lib/models/scanner_model.dart
@@ -10,12 +10,14 @@ class ScannerModel {
     this.valor,
   });
 
+   //Convierte el archivo Json en un modelo ScannerModelo
   factory ScannerModel.fromJson(Map<String, dynamic> json) => ScannerModel(
         id: json["id"],
         tipo: json["tipo"],
         valor: json["valor"],
       );
-
+  
+  //Conviene el modelo en un archivo Json tipo MAP
   Map<String, dynamic> toJson() => {
         "id": id,
         "tipo": tipo,
